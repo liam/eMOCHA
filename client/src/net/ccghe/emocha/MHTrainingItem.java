1ac0bf2b642edca4e99bebf82efb92b8681f486f
Abe Pazos <hamoid@gmail.com>
2009-08-27 18:22:23 +0200

    add training item activity and layout.

---
+++ client/src/net/ccghe/emocha/MHTrainingItem.java
@@ -0,0 +1,16 @@
+package net.ccghe.emocha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MHTrainingItem extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    setContentView(R.layout.mh_training_item);
+	}
+
+}
