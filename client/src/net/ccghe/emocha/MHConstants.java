ee214235d2010a0a163e43c66e951ee4dce46f09
Abe Pazos <hamoid@gmail.com>
2009-08-28 19:01:00 +0200

    Correct copyright headers

--- client/src/net/ccghe/emocha/MHConstants.java
+++ client/src/net/ccghe/emocha/MHConstants.java
@@ -1,15 +1,15 @@
 /*******************************************************************************
- * eMocha - Easy Mobile Open-Source Comprehensive Health Application
+ * eMOCHA - electronic Mobile Open-Source Comprehensive Health Application
  * Copyright (c) 2009 Abe Pazos - abe@ccghe.net
  * 
- * This file is part of eMocha.
+ * This file is part of eMOCHA.
  * 
- * eMocha is free software: you can redistribute it and/or modify
+ * eMOCHA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * eMocha is distributed in the hope that it will be useful,
+ * eMOCHA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
