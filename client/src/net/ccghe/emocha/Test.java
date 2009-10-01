17b621bdcf9ac38c393fe83480afed3e7536c171
Abe Pazos <hamoid@gmail.com>
2009-10-01 19:11:01 +0200

    add license to new latest added files

--- client/src/net/ccghe/emocha/Test.java
+++ client/src/net/ccghe/emocha/Test.java
@@ -1,18 +1,36 @@
+/*******************************************************************************
+ * eMOCHA - electronic Mobile Open-Source Comprehensive Health Application
+ * Copyright (c) 2009 Abe Pazos - abe@ccghe.net
+ * 
+ * This file is part of eMOCHA.
+ * 
+ * eMOCHA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * eMOCHA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package net.ccghe.emocha;
 
-import java.io.File;
 import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import net.ccghe.emocha.model.DBAdapter;
 import net.ccghe.emocha.model.DBAdapter.FileDetails;
 import net.ccghe.utils.FileInfo;
 import net.ccghe.utils.FileUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
