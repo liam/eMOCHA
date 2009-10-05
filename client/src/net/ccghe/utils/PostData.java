33d2e589229f170fec24a23e7c31d303d1098bcf
Abe Pazos <hamoid@gmail.com>
2009-10-05 21:10:19 +0200

    Add named commands and specific functions
    add constants for possible server side operations.
    add functions for doing specific operations in
    the server side, for example activating a phone
    or loading the list of files to put in the sdcard.
    this way we have all server operations centralized,
    and other classes need no knowledge of server
    commands or parameters. response is always a json
    object.

--- client/src/net/ccghe/utils/PostData.java
+++ client/src/net/ccghe/utils/PostData.java
@@ -24,101 +24,108 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.UnknownHostException;
-import java.util.List;
+
+import net.ccghe.emocha.model.Preferences;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
+import android.content.Context;
 import android.util.Log;
 
 public class PostData {
-	public static String Send(List<NameValuePair> tPairs, String tURL) {
-		String tResponseText = "";
-		
-        HttpClient tClient = new DefaultHttpClient();
-        HttpPost   tPost   = new HttpPost(tURL);
-                
+	private static String user;
+	private static String password;
+	private static String serverURL;
+	
+	public static final String CMD_HELLO 				= "hello";
+	public static final String CMD_ACTIVATE_PHONE 		= "activatePhone";
+	public static final String CMD_GET_FILE 			= "getFile";
+	public static final String CMD_GET_SDCARD_FILE_LIST	= "getSdcardFileList";
+	
+	// TODO: retrigger init after changing the settings
+	public static void init(Context context) {
+		serverURL 	= Preferences.getServerURL(context);
+		user 		= Preferences.getUser(context);
+		password 	= Preferences.getPassword(context);
+	}
+	
+	public static JSONObject Send(PostDataPairs pairs) {		
+		HttpClient client = new DefaultHttpClient();
         try {
-        	tPost.setEntity(new UrlEncodedFormEntity(tPairs));
-        	HttpResponse tResponse = tClient.execute(tPost);
+        	HttpPost post = new HttpPost(serverURL); 
+        	post.setEntity(new UrlEncodedFormEntity(pairs.get()));
+        	HttpResponse response = client.execute(post);
         	
-			HttpEntity tEntity   = tResponse.getEntity();
-			InputStream tStream  = tEntity.getContent();
-			tResponseText = convertStreamToString(tStream);
+			HttpEntity entity   = response.getEntity();
+			InputStream stream  = entity.getContent();
+			String jsonResponse = convertStreamToString(stream);			
+			stream.close();
+			if (entity != null) {
+			    entity.consumeContent();
+			}        
+			JSONObject jObject = new JSONObject(jsonResponse);
 			
-			tStream.close();
-			if (tEntity != null) {
-			    tEntity.consumeContent();
-			}        			        	
-        	return tResponseText;
+        	return jObject;
         } catch(ClientProtocolException e) {
         	Log.e("EMOCHA", "ClientProtocolException ERR. " + e.getMessage());
         } catch(UnknownHostException e) {
         	Log.e("EMOCHA", "UnknownHostException ERR. " + e.getMessage());        	
         } catch (IOException e) {
         	Log.e("EMOCHA", "IOException ERR. " + e.getMessage());
-            //e.printStackTrace(); 	
         } catch (Exception e) {
         	Log.e("EMOCHA", "Exception ERR. " + e.getMessage());        	
         }
         return null;
 	}
-	/*
-	public void xPostData(byte[] tData, SharedPreferences tSettings) {
-		     
-        try {
-        	tPost.setEntity(new UrlEncodedFormEntity(tPairs));
-
-        	HttpResponse tResponse = tClient.execute(tPost);
-
-			HttpEntity tEntity = tResponse.getEntity();
-			//int tStatus = tResponse.getStatusLine().getStatusCode();
-			InputStream tStream = tEntity.getContent();
-			String tResponseText = convertStreamToString(tStream);
-			tStream.close();
-			if (tEntity != null) {
-			    tEntity.consumeContent();
-			}        			        	
-        	Log.i("EMOCHA", "Post OK. " + tResponseText);
-        } catch(ClientProtocolException e) {
-        	Log.e("EMOCHA", "Post ERR. " + e.getMessage());
-        } catch (IOException e) {
-        	Log.e("EMOCHA", "Post ERR. " + e.getMessage());
-            e.printStackTrace(); 		
-        }	        
+	public static JSONObject SendAuthenticated(PostDataPairs pairs) {
+		pairs.add("usr", user);
+		pairs.add("pwd", password);
+		return Send(pairs);
 	}
-*/
-	private static String convertStreamToString(InputStream is) {
-        /*
-         * To convert the InputStream to String we use the BufferedReader.readLine()
-         * method. We iterate until the BufferedReader return null which means
-         * there's no more data to read. Each line will appended to a StringBuilder
-         * and returned as String.
-         */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
- 
+	public static JSONObject GetSdcardFileList(String lastServerUpdate) {
+		PostDataPairs data = new PostDataPairs();
+        data.add("last_server_upd", lastServerUpdate);	        
+        data.add("cmd", CMD_GET_SDCARD_FILE_LIST);	        
+		return SendAuthenticated(data);
+	}
+	public static JSONObject activatePhone(String imei) {
+		PostDataPairs data = new PostDataPairs();
+        data.add("imei", imei);	        
+        data.add("cmd", CMD_ACTIVATE_PHONE);
+        return Send(data);		
+	}
+
+    /*
+     * To convert the InputStream to String we use the BufferedReader.readLine()
+     * method. We iterate until the BufferedReader return null which means
+     * there's no more data to read. Each line will appended to a StringBuilder
+     * and returned as String.
+     */
+	private static String convertStreamToString(InputStream stream) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder result = new StringBuilder();
         String line = null;
         try {
             while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
+                result.append(line + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                is.close();
+                stream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
  
-        return sb.toString();
-    } 	
+        return result.toString();
+    } 		
 }
