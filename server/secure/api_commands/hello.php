c258c8c83eab42424ccccd0f5e34c7303d8bedb9
Abe Pazos <hamoid@gmail.com>
2009-10-05 20:50:17 +0200

    hello replies with received post/get vars

--- server/secure/api_commands/hello.php
+++ server/secure/api_commands/hello.php
@@ -1,5 +1,10 @@
 <?
 
-	$responseA['msg'] = 'Hello world :)';
+	$vars = '';
+	foreach($_REQUEST AS $k =>  $v) {
+		$vars .= "$k:$v, ";
+	}
+
+	$responseA['msg'] = 'Hello world :) VARS = '.$vars;
 
 ?>
