e861c6e12ed3f945987a14d381c53bbfb307440a
Abe Pazos <abe@ccghe.net>
2009-10-20 03:05:29 +0200

    add upload table and code, refactor

--- client/src/net/ccghe/emocha/model/DBAdapter.java
+++ client/src/net/ccghe/emocha/model/DBAdapter.java
@@ -38,27 +38,36 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DBAdapter {
-	private static final String TAG = "EMOCHA";
-
 	private static final String DATABASE_NAME 	= "eMOCHA";
-	private static final String SDCARD_TABLE 	= "sdcard";
-	private static final int DATABASE_VERSION 	= 1;
+	private static final int DATABASE_VERSION 	= 2;
+	
+	private static final String TABLE_DOWNLOADS = "downloads";
+	private static final String TABLE_UPLOADS	= "uploads";
 
-	private static final String DATABASE_CREATE = 
-		"CREATE TABLE sdcard ("
+	private static final String TABLE_DOWNLOADS_CREATE = 
+		"CREATE TABLE " + TABLE_DOWNLOADS + " ("
 		+ "path TEXT NOT NULL, " 
 		+ "ts INTEGER, " 
 		+ "size INTEGER, " 
 		+ "md5 TEXT NOT NULL, " 
 		+ "to_delete INTEGER, " 
 		+ "to_download INTEGER);"; 
+	private static final String TABLE_UPLOADS_CREATE = 
+		"CREATE TABLE " + TABLE_UPLOADS + " ("
+		+ "path TEXT NOT NULL, " 
+		+ "ts INTEGER, " 
+		+ "size INTEGER);"; 
 
-	public final static int COLUMN_PATH 		= 0;
-	public final static int COLUMN_TS 			= 1;
-	public final static int COLUMN_SIZE			= 2;
-	public final static int COLUMN_MD5 			= 3;
-	public final static int COLUMN_TO_DELETE 	= 4;
-	public final static int COLUMN_TO_DOWNLOAD 	= 5;
+	public final static int DL_COL_PATH 		= 0;
+	public final static int DL_COL_TS 			= 1;
+	public final static int DL_COL_SIZE			= 2;
+	public final static int DL_COL_MD5 			= 3;
+	public final static int DL_COL_TO_DELETE 	= 4;
+	public final static int DL_COL_TO_DOWNLOAD 	= 5;
+
+	public final static int UL_COL_PATH 		= 0;
+	public final static int UL_COL_TS 			= 1;
+	public final static int UL_COL_SIZE			= 2;
 	
 	private static DBHelper sDBHelper;
 	private static SQLiteDatabase sDB;
@@ -83,22 +92,25 @@ public class DBAdapter {
 
 		@Override
 		public void onCreate(SQLiteDatabase db) {
-			db.execSQL(DATABASE_CREATE);
+			db.execSQL(TABLE_DOWNLOADS_CREATE);
+			db.execSQL(TABLE_UPLOADS_CREATE);
 		}
 
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-			Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
-					+ newVersion + ", which will destroy all old data");
-			db.execSQL("DROP TABLE IF EXISTS sdcard");
-			onCreate(db);
+			Log.w(Constants.LOG_TAG, "Upgrading database from version " + oldVersion + " to " + newVersion);
+			switch(oldVersion) {
+			case 1:
+				db.execSQL("ALTER TABLE sdcard RENAME TO " + TABLE_DOWNLOADS);				
+				db.execSQL(TABLE_UPLOADS_CREATE);
+			}
 		}
 	}
 	
 
 	public static FileInfo getFile(String path) throws SQLException {
 		FileInfo info;
-		Cursor c = sDB.query(true, SDCARD_TABLE, 
+		Cursor c = sDB.query(true, TABLE_DOWNLOADS, 
 				new String[] { "path", "ts", "size", "md5", "to_delete", "to_download" },
 				"path='" + path + "'", null, null, null, null, null);
 		if (c != null) {
@@ -110,12 +122,12 @@ public class DBAdapter {
 	}
 	
 	
-	private static String getFirst(String column, String filter) {
+	private static String getFirst(String column, String filter, String table, int columnId) {
 		String result = null;
-		Cursor c = sDB.query(SDCARD_TABLE, new String[] { column }, filter, null, null, null, null);
+		Cursor c = sDB.query(table, new String[] { column }, filter, null, null, null, null);
 		if(c.getCount() > 0) {
 			c.moveToFirst();
-			result = c.getString(COLUMN_PATH); 
+			result = c.getString(columnId); 
 		}
 		c.close();
 		return result;		
@@ -134,44 +146,44 @@ public class DBAdapter {
 	public static long insertFile(String path, long ts, long size, String md5, boolean delete, boolean download) {		
 		ContentValues values = getValues(ts, size, md5, delete, download);
 		values.put("path", path);
-		return sDB.insert(SDCARD_TABLE, null, values);
+		return sDB.insert(TABLE_DOWNLOADS, null, values);
 	}
 
 	public static boolean deleteFile(String path) {
-		return sDB.delete(SDCARD_TABLE, "path='" + path + "'", null) > 0;
+		return sDB.delete(TABLE_DOWNLOADS, "path='" + path + "'", null) > 0;
 	}
 	public static boolean deleteMarked() {
-		return sDB.delete(SDCARD_TABLE, FILTER_DELETE, null) > 0;		
+		return sDB.delete(TABLE_DOWNLOADS, FILTER_DELETE, null) > 0;		
 	}
 
 	public static boolean updateFile(String path, long ts, long size, String md5, boolean delete, boolean download) {
 		ContentValues values = getValues(ts, size, md5, delete, download);
-		return sDB.update(SDCARD_TABLE, values, "path='" + path + "'", null) > 0;
+		return sDB.update(TABLE_DOWNLOADS, values, "path='" + path + "'", null) > 0;
 	}
 	public static boolean markForDeletion(String path, boolean state) {
 		ContentValues values = new ContentValues();
 		values.put("to_delete", state ? 1 : 0);
-		return sDB.update(SDCARD_TABLE, values, "path='" + path + "'", null) > 0;
+		return sDB.update(TABLE_DOWNLOADS, values, "path='" + path + "'", null) > 0;
 	}
 	public static boolean markForDeletion(boolean state) {
 		ContentValues values = new ContentValues();
 		values.put("to_delete", state ? 1 : 0);
-		return sDB.update(SDCARD_TABLE, values, null, null) > 0;
+		return sDB.update(TABLE_DOWNLOADS, values, null, null) > 0;
 	}
 	public static boolean markForDownload(String path, String newMD5) {
 		ContentValues values = new ContentValues();
 		values.put("to_download", 1);
 		values.put("md5", newMD5);
-		return sDB.update(SDCARD_TABLE, values, "path='" + path + "'", null) > 0;
+		return sDB.update(TABLE_DOWNLOADS, values, "path='" + path + "'", null) > 0;
 	}
 	
 	public static Cursor getFiles() {
-		return sDB.query(SDCARD_TABLE, 
+		return sDB.query(TABLE_DOWNLOADS, 
 				new String[] { "path", "ts", "size", "md5", "to_delete", "to_download"}, 
 				null, null, null, null, null);
 	}
 	public static ArrayList<String> getFilePaths() {
-		Cursor c = sDB.query(SDCARD_TABLE, 
+		Cursor c = sDB.query(TABLE_DOWNLOADS, 
 				new String[] { "path"}, 
 				null, null, null, null, null);
 		ArrayList<String> result = new ArrayList<String>();
@@ -179,36 +191,45 @@ public class DBAdapter {
 		int numRows = c.getCount();
 		for (int i = 0; i < numRows; i++) {
 			c.moveToPosition(i);
-			result.add(c.getString(COLUMN_PATH));
+			result.add(c.getString(DL_COL_PATH));
 		}		
 		c.close();
 		return result;
 	}
-	public static int pendingDownloadNum() {
-		int result;
-		Cursor c = sDB.query(SDCARD_TABLE, 
-				new String[] { "1" }, 
+	public static int pendingFileTransfersNum() {
+		int result = 0;
+		Cursor c;
+		
+		c = sDB.query(TABLE_DOWNLOADS, new String[] { "1" }, 
 				FILTER_DOWNLOAD, null, null, null, null);
-		if (c == null) {
-			result = 0;
-		} else {
-			result = c.getCount();
+		if (c != null) {
+			result += c.getCount();
+		}
+		c.close();
+
+		c = sDB.query(false, TABLE_UPLOADS, new String[] { "1" }, 
+				null, null, null, null, null, null);
+		if (c != null) {
+			result += c.getCount();
 		}
 		c.close();
 		return result;
 	}
 	public static String getFirstDownloadID() {
-		return getFirst("path", FILTER_DOWNLOAD);
+		return getFirst("path", FILTER_DOWNLOAD, TABLE_DOWNLOADS, DL_COL_PATH);
+	}
+	public static String getFirstUploadID() {
+		return getFirst("path", null, TABLE_UPLOADS, UL_COL_PATH);
 	}
 	public static ArrayList<String> getFilesFiltered(String filter) {
 		ArrayList<String> result = new ArrayList<String>();
-		Cursor c = sDB.query(SDCARD_TABLE, new String[] { "path"}, filter, null, null, null, null);
+		Cursor c = sDB.query(TABLE_DOWNLOADS, new String[] { "path"}, filter, null, null, null, null);
 		int numRows = c.getCount();
 		Log.i(Constants.LOG_TAG, filter + " : " + numRows);
 		for (int i = 0; i < numRows; i++) {
 			c.moveToPosition(i);
-			result.add(c.getString(COLUMN_PATH));
-			Log.i(Constants.LOG_TAG, i + " : " + c.getString(COLUMN_PATH));
+			result.add(c.getString(DL_COL_PATH));
+			Log.i(Constants.LOG_TAG, i + " : " + c.getString(DL_COL_PATH));
 		}
 		c.close();
 		return result;
@@ -277,17 +298,38 @@ public class DBAdapter {
 		ContentValues values = new ContentValues();
 		values.put("to_download", 0);
 				
-		int affected = sDB.update(SDCARD_TABLE, values, 
+		int affected = sDB.update(TABLE_DOWNLOADS, values, 
 				"path='" + file.path() + "' AND md5='" + file.md5() + "'", null);
 		
 		Log.i(Constants.LOG_TAG, "Mark as downloaded: " + file.path() + 
 				" (" + file.md5() + ") " + affected);
 	}
+	public static boolean markAsUploaded(String path) {
+		return sDB.delete(TABLE_UPLOADS, "path='" + path + "'", null) > 0;
+	}
 	public static int clean() {
 		ContentValues values = new ContentValues();
 		values.put("to_download", 0);
 				
-		return sDB.update(SDCARD_TABLE, values, null, null);
+		return sDB.update(TABLE_DOWNLOADS, values, null, null);
+	}
+
+	public static long insertFilesNewerThan(Long lastUploadTimestamp,
+			ArrayList<FileInfo> sdcardFiles) {			
+
+		Long newUploadTimeStamp = lastUploadTimestamp; 
+		for(FileInfo sdcardFile : sdcardFiles) {
+			if (sdcardFile.lastModified() > lastUploadTimestamp) {
+				ContentValues values = new ContentValues(3);
+				values.put("path",	sdcardFile.path());
+				values.put("ts", 	sdcardFile.lastModified());
+				values.put("size", 	sdcardFile.length());
+				sDB.insert(TABLE_UPLOADS, null, values);
+				
+				newUploadTimeStamp = Math.max(newUploadTimeStamp, sdcardFile.lastModified());
+			}
+		}
+		return newUploadTimeStamp;
 	}
 	
 }
