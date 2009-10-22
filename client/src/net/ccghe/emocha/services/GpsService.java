e5551de2bd20c4838ea486565c188d29186e9040
Jiri <jiri@jiri.local>
2009-10-22 14:47:45 +0200

    First commit from Jiri It turned out that the fix attempts number was to high, resulting in continues gps fixing attempts. I have set it back to 3 fixes. I also made the gps update time a bit higher, it will now attempt to do a fix if the time difference between the last location its timestamp is greater then 60 seconds. Added a description of the service class.

--- client/src/net/ccghe/emocha/services/GpsService.java
+++ client/src/net/ccghe/emocha/services/GpsService.java
@@ -16,29 +16,38 @@ import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
-
+/**
+ * 
+ * @author jiri
+ * This service is not a service class in the strict sense that it runs by itself in the background.
+ * This class registers itself to an intent filter so that we can call it to update the gps position.
+ * When this service services the intent it checks if the timestamp
+ * of the last location to the current system time. If the difference is greater then GPS_UPDATE_MS it attaches
+ * a listener to the gps. 
+ * In case of poor reception of the GPS and therefor no fix can be made the service will try MAX_FIX_ATTEMPS to get a fix. If it does
+ * not succeed it sets the gps location to null. This forces the services to do another check, despite the time difference being smaller
+ * then GPS_UPDATE_MS, when it receives an intent to update the gps.
+ */
 public class GpsService extends Service {
 
     public static final String      INTENT_FILTER    = "net.ccghe.emocha.GPS_LOCATION_UPDATE";
-
-//    private static final String     LONGITUDE        = "lng";
-//    private static final String     LATTITUDE        = "lat";
-//    private static final String     TIME             = "time";
-    private static final int        MAX_FIX_ATTEMPS  = 6;
+    
+    private static final int        MAX_FIX_ATTEMPS  = 3;
     private static final String     GPS_PROVIDER_ID  = LocationManager.GPS_PROVIDER;
-    private static final long       GPS_UPDATE_MS    = 30 * Constants.ONE_SECOND;
+    private static final long       GPS_UPDATE_MS    = 60 * Constants.ONE_SECOND;
+    private static final boolean    LOG_TO_TEXT      = true;
 
     private LocationManager         mLocationManager = null;
     private Location                mLocation;
     private LocationListener        mLocationListener;
     private Boolean                 mPendingUpdate   = false;
-    
+
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-	@Override
-	public void onReceive(Context context, Intent intent) {
-	    updateGps();
-	}
-    };
+         @Override
+         public void onReceive(Context context, Intent intent) {
+             updateGps();
+         }
+     };
 
     @Override
     public void onDestroy() {
@@ -47,22 +56,20 @@ public class GpsService extends Service {
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        IntentFilter intentFilter = new IntentFilter(GpsService.INTENT_FILTER);
-        this.registerReceiver(mReceiver, intentFilter);
-    }
-
-    @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
+        
+        IntentFilter intentFilter = new IntentFilter(GpsService.INTENT_FILTER);
+        this.registerReceiver(mReceiver, intentFilter);
+        
         mLocationManager = (LocationManager) this.getBaseContext().getSystemService(Context.LOCATION_SERVICE);
         mLocationListener = new DeviceLocationListener();
         mLocation = mLocationManager.getLastKnownLocation(GPS_PROVIDER_ID);
     }
+
     /**
-     * @param loc   Can be set to null, so that we force a gps fixing attempt on the next call of
-     *              updateGps, discarding the time passed.
+     * @param   loc    Can be set to null, so that we force a gps fixing attempt on
+     *                 the next call of updateGps, discarding the time passed.
      */
     private void setGps(Location loc) {
         mLocationManager.removeUpdates(mLocationListener);
@@ -71,83 +78,84 @@ public class GpsService extends Service {
     }
 
     private void updateGps() {
-	if (!mPendingUpdate) {
-	    Long timestamp = (mLocation == null) ? -1 : System.currentTimeMillis() - mLocation.getTime();
+        if (!mPendingUpdate) {
+            Long timestamp = (mLocation == null) ? -1 : System.currentTimeMillis()- mLocation.getTime();
 
-	    if (timestamp == -1 || timestamp > GPS_UPDATE_MS) {
-		Log.i("EMOCHA", "Start a gps update listener");
-		mLocationManager.requestLocationUpdates(GPS_PROVIDER_ID, 0, 0, mLocationListener);
-		mPendingUpdate = true;
-	    }
-	}
+            if (timestamp == -1 || timestamp > GPS_UPDATE_MS) {
+                Log.i( getClass().getSimpleName() , "Start a gps update listener");
+                mLocationManager.requestLocationUpdates(GPS_PROVIDER_ID, 0, 0, mLocationListener);
+                mPendingUpdate = true;
+            }
+        }
 
     }
 
     public class DeviceLocationListener implements LocationListener {
-	private int mFixAttempts = 0;
+        private int mFixAttempts = 0;
 
-	public DeviceLocationListener() {
-	    appendLog(" DeviceLocationListener inistantiated ");
-	}
+        public DeviceLocationListener() {
+            appendLog(" DeviceLocationListener inistantiated ");
+        }
 
-	public void onLocationChanged(Location location) {
-	    double lat = location.getLatitude();
-	    double lng = location.getLongitude();
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
 
-	    appendLog(" loc : [ " + lat + ":" + lng + " ]\n");
-	    onGpsResult(location);
-	}
+            appendLog(" loc : [ " + lat + ":" + lng + " ]\n");
+            onGpsResult(location);
+        }
 
-	private void appendLog(String msg) {
-	    try {
-		FileLogUtil.appendToLog("gpsdata.txt", msg);
-	    } catch (IOException e) {
-	    }
-	}
+        private void appendLog(String msg) {
+            if(!LOG_TO_TEXT) return;
+            
+            try {
+                FileLogUtil.appendToLog("gpsdata.txt", msg);
+            } catch (IOException ignore) {}
+        }
 
-	public void onProviderDisabled(String provider) {
-	    onGpsResult(null);
-	    appendLog(" onProviderDisabled");
-	}
+        public void onProviderDisabled(String provider) {
+            onGpsResult(null);
+            appendLog(" onProviderDisabled");
+        }
 
-	public void onProviderEnabled(String provider) {
-	    appendLog(" onProviderEnabled");
-	    updateGps();
-	}
+        public void onProviderEnabled(String provider) {
+            appendLog(" onProviderEnabled");
+            updateGps();
+        }
 
-	private void onGpsResult(Location location) {
-	    mFixAttempts = 0;
-	    setGps(location);
-	    Log.i("EMOCHA", "Gps result " + ((location == null) ? "null" : "location is set"));
-	}
+        private void onGpsResult(Location location) {
+            mFixAttempts = 0;
+            setGps(location);
+            Log.i(getClass().getSimpleName(), "Gps result " + ((location == null) ? "null" : "location is set"));
+        }
 
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	    String stat = "undefined";
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            String stat = "undefined";
 
-	    switch (status) {
-	    case LocationProvider.AVAILABLE:
-		stat = "AVAILABLE";
-		break;
-	    case LocationProvider.OUT_OF_SERVICE:
-		stat = "OUT_OF_SERVICE";
-		onGpsResult(null);
-		break;
-	    case LocationProvider.TEMPORARILY_UNAVAILABLE:
-		stat = "TEMPORARILY_UNAVAILABLE";
-		Log.w("EMOCHA", "attempt to fix gps " + mFixAttempts);
-		if (mFixAttempts++ >= MAX_FIX_ATTEMPS) {
-		    onGpsResult(null);
-		    appendLog(" Provider: " + provider + " could not get a fix because status: " + stat);
+            switch (status) {
+            case LocationProvider.AVAILABLE:
+                stat = "AVAILABLE";
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                stat = "OUT_OF_SERVICE";
+                onGpsResult(null);
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                stat = "TEMPORARILY_UNAVAILABLE";
+                Log.w(getClass().getSimpleName(), "attempt to fix gps " + mFixAttempts);
+                if (mFixAttempts++ >= MAX_FIX_ATTEMPS) {
+                    onGpsResult(null);
+                    appendLog(" Provider: " + provider  + " could not get a fix because status: " + stat);
 
-		}
-		break;
-	    }
-	}
+                }
+                break;
+            }
+        }
 
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-	return null;
+        return null;
     }
 }
