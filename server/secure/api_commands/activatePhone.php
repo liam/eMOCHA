dc039bb234c9863b3592182794cd70b1f8f98e3d
Abe Pazos <abe@ccghe.net>
2009-10-24 14:40:04 +0200

    when requesting a new phone activation store timestamp and ip

--- server/secure/api_commands/activatePhone.php
+++ server/secure/api_commands/activatePhone.php
@@ -1,10 +1,29 @@
 <?php
 
-	$imei = preg_replace('/\W/', '', $_REQUEST['imei']); 
+	function getIP(){
+	    if( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] )) 
+	    	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	    else if( isset( $_SERVER ['HTTP_VIA'] ))  
+	    	$ip = $_SERVER['HTTP_VIA'];
+	    else if( isset( $_SERVER ['REMOTE_ADDR'] ))  
+	    	$ip = $_SERVER['REMOTE_ADDR'];
+	    else 
+	    	$ip = null ;
+	    
+	    return $ip;
+	}
+
+	$imei = preg_replace('/\W/', '', $_REQUEST['imei']);
+	$ip = getIP(); 
 
 	if (strlen($imei) > 14) {
 		query("INSERT INTO phone SET ".
-			"imei='$imei', imei_md5=MD5('$imei'), validated=0, last_connect_ts=UNIX_TIMESTAMP()");
+			"imei='$imei', ".
+			"imei_md5=MD5('$imei'), ".
+			"validated=0, ".
+			"creation_ts=UNIX_TIMESTAMP(), ".
+			"creation_ip='$ip'"
+		);
 		$responseA['msg'] = 'Phone activation sent. The administrator will contact you.';
 	} else {
 		$responseA['msg'] = 'Server says IMEI code not received!';		
