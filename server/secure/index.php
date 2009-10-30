4ba5e600ca4fb3ef3e1b29979154f9e908fe5565
Abe Pazos <abe@ccghe.net>
2009-10-30 10:43:36 -0400

    tabs must be clicked now, not rolled over

--- server/secure/index.php
+++ server/secure/index.php
@@ -68,9 +68,7 @@
 	<script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
 	<script type="text/javascript">
 		$(function() {
-		    $("#tabs").tabs({
-		    	event: 'mouseover'
-		    });
+		    $("#tabs").tabs();
 		    //select tab on start up
 		    <? if ($_REQUEST['sec']) { ?>
 			$("#tabs").tabs('select', <? print $_REQUEST['sec']; ?>);
